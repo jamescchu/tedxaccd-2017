bc90f15746b79bb981cdbb79568611a97005dbcc
James Chu <james@james-chu.com>
2017-06-11 13:52:06 -0700

    implement barba pjax

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,22 @@ const parallax = new Parallax(scene, {
   scalarY: 4
 });
 import Barba from "barba.js";
+const dom = Barba.Pjax.Dom;
+dom.wrapperId = "js-wrapper";
+dom.containerClass = "js-container";
+
+document.addEventListener("DOMContentLoaded", () => {
+    Barba.Pjax.init();
+    Barba.Prefetch.init();
+
+    // Scroll to the wrapper
+    Barba.Dispatcher.on('transitionCompleted', () => {
+        window.scrollTo(0, 0);
+        initPhotoSwipeFromDOM(gallerySelector);
+    });
+
+    // Add transition to barba
+    Barba.Pjax.getTransition = () => Transition;
+
+    Barba.transitionLength = 250;
+});
