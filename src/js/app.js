48b04f9394990d32a3d184c14e6177116246ce5d
James Chu <james@james-chu.com>
2017-06-11 13:54:10 -0700

    remove parallax

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,11 @@
 // JS Goes here - ES6 supported
-import Parallax from "parallax-js";
-const scene = document.getElementById('scene');
-const parallax = new Parallax(scene, {
-  scalarX: 4,
-  scalarY: 4
-});
+// import Parallax from "parallax-js";
+// const scene = document.getElementById('scene');
+// const parallax = new Parallax(scene, {
+//   scalarX: 4,
+//   scalarY: 4
+// });
+
 import Barba from "barba.js";
 const dom = Barba.Pjax.Dom;
 dom.wrapperId = "js-wrapper";
