f38b9912f26c43e150f368d2dde65203dc5afbf9
James Chu <james@james-chu.com>
2017-07-16 17:17:26 -0700

    removed parallax

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,1 @@
 // JS Goes here - ES6 supported
-import Parallax from "parallax-js";
-const scene = document.getElementById('scene');
-const parallax = new Parallax(scene, {
-  scalarX: 4,
-  scalarY: 4
-});
