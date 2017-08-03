f643ab75203b056552256c31f954402f7b643435
James Chu <james@james-chu.com>
2017-08-02 17:03:47 -0700

    added google maps

--- src/js/app.js
+++ src/js/app.js
@@ -30,6 +30,28 @@ Barba.Dispatcher.on('transitionCompleted', () => {
   document.getElementById("id__menu-state").checked = false;
 });
 
+const logotype = document.getElementById("js--logotype");
+const nav = document.getElementById("js--nav");
+let scrolled = false;
+const stickPoint = getDistance();
+
+function getDistance() {
+  const topDist = logotype.offsetTop;
+  return topDist;
+}
+
+window.onscroll = e => {
+  const distance = getDistance() - window.pageYOffset;
+  const offset = window.pageYOffset;
+  if (( distance <= 0) && !scrolled) {
+    logotype.classList.add('logotype--scrolled');
+    scrolled = true;
+  } else if (scrolled && (offset <= stickPoint)) {
+    logotype.classList.remove('logotype--scrolled');
+    scrolled = false;
+  }
+}
+
 // document.addEventListener("DOMContentLoaded", () => {
 //     Barba.Pjax.init();
 //     Barba.Prefetch.init();
