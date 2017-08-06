026b4a4d6d8345d47caf35fbdef9ffbe774abce5
James Chu <james@james-chu.com>
2017-08-05 22:18:49 -0700

    replaced fullPage with something more lightweight

--- src/js/app.js
+++ src/js/app.js
@@ -1,37 +1,9 @@
-// JS Goes here - ES6 supported
-// import Parallax from "parallax-js";
-// const scene = document.getElementById('scene');
-// const parallax = new Parallax(scene, {
-//   scalarX: 4,
-//   scalarY: 4
-// });
-
 import Barba from "barba.js";
-
-// const links = document.querySelectorAll('a[href]');
-// const cbk = e => {
-//  if(e.currentTarget.href === window.location.href) {
-//    e.preventDefault();
-//    e.stopPropagation();
-//  }
-// };
-//
-// for(let i = 0; i < links.length; i++) {
-//   links[i].addEventListener('click', cbk);
-// }
 
 const dom = Barba.Pjax.Dom;
 dom.wrapperId = "js--wrapper";
 dom.containerClass = "js--container";
 
-// function scriptc(a,b){
-//   var __d=document;
-//   var __h = __d.getElementsByTagName("head")[0];
-//   var s = __d.createElement("script");
-//   s.setAttribute("src", a);
-//   s.id = b;
-//   __h.appendChild(s);
-// }
 
 // Barba.Pjax.start();
 // Barba.Prefetch.init();
@@ -46,28 +18,6 @@ Barba.Dispatcher.on('transitionCompleted', () => {
   }
 });
 const logotype = document.getElementById("js--logotype");
-// const nav = document.getElementById("js--header");
-// let scrolled = false;
-// const stickPoint = getDistance();
-//
-// function getDistance() {
-//   const topDist = nav.offsetTop + nav.offsetHeight;
-//   return topDist;
-// }
-
-// window.onscroll = e => {
-//   const distance = getDistance() - window.pageYOffset;
-//   const offset = window.pageYOffset;
-//   if (( distance <= 0) && !scrolled) {
-//     logotype.classList.add('logotype--scrolled');
-//     scrolled = true;
-//   } else if (scrolled && (offset <= stickPoint)) {
-//     logotype.classList.remove('logotype--scrolled');
-//     scrolled = false;
-//   }
-// }
-
-
 const menuIcon = document.getElementById("js--menu-icon");
 const navMenu = document.getElementById("js--nav");
 
@@ -207,6 +157,15 @@ Barba.Pjax.getTransition = function() {
   return Transition;
 };
 
-$(document).ready(function() {
-  $('#js--page').fullpage();
+onePageScroll("#js--wrapper", {
+  sectionContainer: "section",
+   easing: "ease",
+   animationTime: 1000,
+   pagination: true,
+   updateURL: false,
+   beforeMove: function(index) {},
+   afterMove: function(index) {},
+   loop: false,
+   keyboard: true,
+   responsiveFallback: false
 });
