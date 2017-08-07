01ce394228fec6fd213ec52525fbb3c9ea1d78db
James Chu <james@james-chu.com>
2017-08-07 16:28:27 -0700

    adjusted type size + imported rellax + headroom variables

--- src/js/app.js
+++ src/js/app.js
@@ -1,37 +1,12 @@
 // JS Goes here - ES6 supported
-// import Parallax from "parallax-js";
-// const scene = document.getElementById('scene');
-// const parallax = new Parallax(scene, {
-//   scalarX: 4,
-//   scalarY: 4
-// });
 
 import Barba from "barba.js";
-
-// const links = document.querySelectorAll('a[href]');
-// const cbk = e => {
-//  if(e.currentTarget.href === window.location.href) {
-//    e.preventDefault();
-//    e.stopPropagation();
-//  }
-// };
-//
-// for(let i = 0; i < links.length; i++) {
-//   links[i].addEventListener('click', cbk);
-// }
+import Rellax from "./rellax.js";
 
 const dom = Barba.Pjax.Dom;
 dom.wrapperId = "js--wrapper";
 dom.containerClass = "js--container";
 
-// function scriptc(a,b){
-//   var __d=document;
-//   var __h = __d.getElementsByTagName("head")[0];
-//   var s = __d.createElement("script");
-//   s.setAttribute("src", a);
-//   s.id = b;
-//   __h.appendChild(s);
-// }
 
 Barba.Pjax.start();
 Barba.Prefetch.init();
@@ -46,26 +21,6 @@ Barba.Dispatcher.on('transitionCompleted', () => {
   }
 });
 const logotype = document.getElementById("js--logotype");
-// const nav = document.getElementById("js--header");
-// let scrolled = false;
-// const stickPoint = getDistance();
-//
-// function getDistance() {
-//   const topDist = nav.offsetTop + nav.offsetHeight;
-//   return topDist;
-// }
-
-// window.onscroll = e => {
-//   const distance = getDistance() - window.pageYOffset;
-//   const offset = window.pageYOffset;
-//   if (( distance <= 0) && !scrolled) {
-//     logotype.classList.add('logotype--scrolled');
-//     scrolled = true;
-//   } else if (scrolled && (offset <= stickPoint)) {
-//     logotype.classList.remove('logotype--scrolled');
-//     scrolled = false;
-//   }
-// }
 
 
 const menuIcon = document.getElementById("js--menu-icon");
@@ -75,6 +30,10 @@ const navMenu = document.getElementById("js--nav");
 var header = document.querySelector("header");
 // construct an instance of Headroom, passing the element
 var headroom  = new Headroom(header, {
+  tolerance: {
+    up: 10,
+    down: 5
+  },
   classes: {
     inital: "header",
     pinned: "header--hide",
@@ -206,38 +165,3 @@ var Transition = Barba.BaseTransition.extend({
 Barba.Pjax.getTransition = function() {
   return Transition;
 };
-
-
-// const fadeTransition = Barba.BaseTransition.extend({
-//   start() {
-//     Promise
-//       .all([this.newContainerLoading, this.fadeOut()])
-//       .then(this.fadeIn.bind(this));
-//   },
-//
-//   fadeOut() {
-//     return this.oldContainer.classList.add("js--fade-out").promimse();
-//   },
-//
-//   fadeIn() {
-//     return this.newContainer.classList.add("js--fade-in").done();
-//   }
-// });
-//
-// Barba.Pjax.getTransition = () =>
-// FadeTransition;
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     Barba.Pjax.init();
-//     Barba.Prefetch.init();
-//
-//     // Scroll to the wrapper
-//     Barba.Dispatcher.on('transitionCompleted', () => {
-//         window.scrollTo(0, 0);
-//     });
-//
-//     // Add transition to barba
-//     Barba.Pjax.getTransition = () => Transition;
-//
-//     Barba.transitionLength = 250;
-// });
