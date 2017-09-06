824beed7e6195d5d404736e5b113c629c237ce5c
James Chu <james@james-chu.com>
2017-09-06 23:17:06 +0800

    refactored js to be a bit more consistent and properly formatted

--- src/js/app.js
+++ src/js/app.js
@@ -1,340 +1,291 @@
-// JS Goes here - ES6 supported
-
-import Barba from "barba.js";
+import Barba from 'barba.js'
 // import Headroom from "./headroom.min.js";
 // import Rellax from "./rellax.js";
 // import ScrollTrigger from "./ScrollTrigger.js";
-import AOS from "./aos.js";
-import MoveTo from "./moveTo.js";
-import VanillaModal from "vanilla-modal";
-import Plyr from "plyr";
+import AOS from './aos.js'
+import MoveTo from './moveTo.js'
+import VanillaModal from 'vanilla-modal'
+import Plyr from 'plyr'
 
-const moveTo = new MoveTo();
-const dom = Barba.Pjax.Dom;
-const plyr = Plyr;
+const moveTo = new MoveTo()
+const dom = Barba.Pjax.Dom
+const plyr = Plyr
 
-dom.wrapperId = "js--wrapper";
-dom.containerClass = "js--container";
-Barba.Pjax.ignoreClassLink = "pjax--disable";
+dom.wrapperId = 'js--wrapper'
+dom.containerClass = 'js--container'
+Barba.Pjax.ignoreClassLink = 'pjax--disable'
 
 function refreshLoad() {
-  setMetrics();
-  loadMap();
+  setMetrics()
+  loadMap()
 
-  const triggerMove = document.getElementsByClassName('js--trigger');
+  const triggerMove = document.getElementsByClassName('js--trigger')
 
   plyr.setup({
-    controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'fullscreen'],
+    controls: [
+      'play-large',
+      'play',
+      'progress',
+      'current-time',
+      'mute',
+      'volume',
+      'fullscreen',
+    ],
   })
 
   const modal = new VanillaModal({
     clickOutside: true,
-  });
+  })
 
-  for (var i = 0; i < triggerMove.length; i++) {
-    moveTo.registerTrigger(triggerMove[i]);
+  for (let i = 0; i < triggerMove.length; i++) {
+    moveTo.registerTrigger(triggerMove[i])
   }
-  if (window.location.href.indexOf("#promo") > -1) {
-    modal.open('#promo');
-  } else if (location.pathname == "/") {
-    downIcon.classList.add('show-icon');
-    downIcon.classList.remove('hide-icon');
-  }else {
-    downIcon.classList.remove('show-icon');
-    downIcon.classList.add('hide-icon');
+  if (window.location.href.includes('#promo')) {
+    modal.open('#promo')
+  } else if (location.pathname == '/') {
+    downIcon.classList.add('show-icon')
+    downIcon.classList.remove('hide-icon')
+  } else {
+    downIcon.classList.remove('show-icon')
+    downIcon.classList.add('hide-icon')
   }
 }
 
-document.addEventListener('DOMContentLoaded', function(){
-  Barba.Pjax.start();
-  Barba.Prefetch.init();
-  AOS.init();
+document.addEventListener('DOMContentLoaded', () => {
+  Barba.Pjax.start()
+  Barba.Prefetch.init()
+  AOS.init()
 
   //refreshLoad();
-});
+})
 
 Barba.Dispatcher.on('newPageReady', () => {
-  if ( isNavVisible(navMenu) ) {
-    navMenu.classList.remove('nav--active');
-    menuIcon.classList.remove('is-active');
+  if (isNavVisible(navMenu)) {
+    navMenu.classList.remove('nav--active')
+    menuIcon.classList.remove('is-active')
   }
-});
+})
 
 Barba.Dispatcher.on('transitionCompleted', () => {
   // document.getElementById("id__menu-state").checked = false;
-   AOS.refresh();
+  AOS.refresh()
 
-   refreshLoad();
-});
-const logotype = document.getElementById("js--logotype");
+  refreshLoad()
+})
+const logotype = document.getElementById('js--logotype')
 
-const menuIcon = document.getElementById("js--menu-icon");
-const navMenu = document.getElementById("js--nav");
+const menuIcon = document.getElementById('js--menu-icon')
+const navMenu = document.getElementById('js--nav')
 
-const upIcon = document.getElementById("js--up-icon");
-const downIcon = document.getElementById("js--down-icon");
+const upIcon = document.getElementById('js--up-icon')
+const downIcon = document.getElementById('js--down-icon')
 
 // grab an element
-var header = document.querySelector("header");
+const header = document.querySelector('header')
 
-let stuck = false;
-const stickPoint = getDistance();
+let stuck = false
+const stickPoint = getDistance()
 
 function getDistance() {
-  const topDist = logotype.offsetTop;
-  return topDist;
+  const topDist = logotype.offsetTop
+  return topDist
 }
 
 function scrollSwitch() {
-  ticking = false;
-  const distance = getDistance() - window.pageYOffset;
-  const offset = window.pageYOffset;
-  if ( (distance <= 0) && !stuck) {
-      logotype.classList.add('logotype--scrolled');
-      upIcon.classList.add('show-icon');
-      upIcon.classList.remove('hide-icon');
-      if (location.pathname == "/") {
-        downIcon.classList.add('hide-icon');
-      }
-      stuck = true;
-    } else if (stuck && (offset <= stickPoint)){
-      logotype.classList.remove('logotype--scrolled');
-      upIcon.classList.remove('show-icon');
-      upIcon.classList.add('hide-icon');
-      if (location.pathname == "/") {
-        downIcon.classList.add('show-icon');
-        downIcon.classList.remove('hide-icon');
-      }
-      stuck = false;
+  ticking = false
+  const distance = getDistance() - window.pageYOffset
+  const offset = window.pageYOffset
+  if (distance <= 0 && !stuck) {
+    logotype.classList.add('logotype--scrolled')
+    upIcon.classList.add('show-icon')
+    upIcon.classList.remove('hide-icon')
+    if (location.pathname == '/') {
+      downIcon.classList.add('hide-icon')
     }
-
+    stuck = true
+  } else if (stuck && offset <= stickPoint) {
+    logotype.classList.remove('logotype--scrolled')
+    upIcon.classList.remove('show-icon')
+    upIcon.classList.add('hide-icon')
+    if (location.pathname == '/') {
+      downIcon.classList.add('show-icon')
+      downIcon.classList.remove('hide-icon')
+    }
+    stuck = false
+  }
 }
-// construct an instance of Headroom, passing the element
-// var headroom  = new Headroom(header, {
-//   tolerance: {
-//     up: 10,
-//     down: 5
-//   },
-//   classes: {
-//     inital: "header",
-//     pinned: "header--hide",
-//     unpinned: "header--show",
-//     top: "header--top",
-//     notTop: "header--offset",
-//     bottom: "header--btm",
-//     notBottom: "header--float"
-//   },
-//   onUnpin: function() {
-//     logotype.classList.add('logotype--scrolled');
-//     progressBar.classList.remove('logotype__progress--scrolled');
-//     push.classList.remove('logotype__progress--scrolled');
-//     if ( isNavVisible(navMenu) ) {
-//       this.elem.classList.remove(this.classes.unpinned);
-//       this.elem.classList.add(this.classes.pinned);
-//       logotype.classList.remove('logotype--scrolled');
-//       progressBar.classList.add('logotype__progress--scrolled');
-//       push.classList.add('logotype__progress--scrolled');
-//     }
-//     else {
-//       this.elem.classList.add(this.classes.unpinned);
-//       this.elem.classList.remove(this.classes.pinned);
-//     }
-//   },
-//   onPin: function() {
-//     logotype.classList.remove('logotype--scrolled');
-//     progressBar.classList.add('logotype__progress--scrolled');
-//     push.classList.add('logotype__progress--scrolled');
-//
-//   }
-// });
-// initialise
-//headroom.init();
 
 function isNavVisible(nav) {
-  return ( nav.classList.contains('nav--active') ? true : false );
+  return nav.classList.contains('nav--active') ? true : false
 }
 
-menuIcon.onclick = function() {
-    menuIcon.classList.toggle("is-active");
-    navMenu.classList.toggle("nav--active");
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('is-active')
+  navMenu.classList.toggle('nav--active')
 }
 
 function initMap() {
-  var locationACCD = {lat: 34.168871, lng: -118.185157};
-  var map = new google.maps.Map(document.getElementById('js--map'), {
+  const locationACCD = { lat: 34.168871, lng: -118.185157 }
+  const map = new google.maps.Map(document.getElementById('js--map'), {
     zoom: 17,
     center: locationACCD,
     gestureHandling: 'cooperative',
-    scrollwheel: false
-  });
-  var marker = new google.maps.Marker({
+    scrollwheel: false,
+  })
+  const marker = new google.maps.Marker({
     position: locationACCD,
-    map: map,
-    title: 'ArtCenter College of Design'
-  });
-};
+    map,
+    title: 'ArtCenter College of Design',
+  })
+}
 
 function loadMap() {
-  if (window.location.href.indexOf("venue") != -1) {
+  if (window.location.href.includes('venue')) {
     initMap()
   }
-};
+}
 
 // Define barba properties
-Barba.transitionLength = 250;
+Barba.transitionLength = 250
 
 // Define transition
-var Transition = Barba.BaseTransition.extend({
-  start: function() {
-    this.newContainerLoading.then(this.runTransition.bind(this));
+const Transition = Barba.BaseTransition.extend({
+  start() {
+    this.newContainerLoading.then(this.runTransition.bind(this))
   },
 
-  runTransition: function() {
-
-    document.body.style.overflow = 'hidden';
+  runTransition() {
+    document.body.style.overflow = 'hidden'
 
     ////////////////////////////
     // Setup
     ////////////////////////////
-    var transitionLength = parseInt(Barba.transitionLength),
-        transitionTimeout = 100,
-        transitionLengthSeconds = (transitionLength / 1000) + 's',
-        transitionSelector = 'data-transition';
+    const transitionLength = parseInt(Barba.transitionLength)
+
+    const transitionTimeout = 100
+    const transitionLengthSeconds = `${transitionLength / 1000}s`
+    const transitionSelector = 'data-transition'
     ////////////////////////////
 
     // Set the animation time on all elements
-    var allAnimationElements = document.querySelectorAll('[' + transitionSelector + ']');
+    const allAnimationElements = document.querySelectorAll(
+      `[${transitionSelector}]`
+    )
     for (var i = 0; i < allAnimationElements.length; i++) {
-      element = allAnimationElements[i];
+      element = allAnimationElements[i]
 
       // Set styles
-      element.style.animationDuration = transitionLengthSeconds;
-      element.style.animationDelay = transitionLengthSeconds;
-      element.style.animationName = element.dataset.transition;
-      element.style.animationFillMode = 'forwards';
+      element.style.animationDuration = transitionLengthSeconds
+      element.style.animationDelay = transitionLengthSeconds
+      element.style.animationName = element.dataset.transition
+      element.style.animationFillMode = 'forwards'
     }
 
     // Get all old elements with transitions
-    var oldElements = this.oldContainer.querySelectorAll('[' + transitionSelector + ']');
+    const oldElements = this.oldContainer.querySelectorAll(
+      `[${transitionSelector}]`
+    )
     for (var i = 0; i < oldElements.length; i++) {
-      var element = oldElements[i];
+      var element = oldElements[i]
 
       // Remove style tag
-      element.removeAttribute('style');
+      element.removeAttribute('style')
     }
 
     // Trigger out transitions
-    setTimeout(function(){
-
-      for (var i = 0; i < oldElements.length; i++) {
-        element = oldElements[i];
+    setTimeout(() => {
+      for (let i = 0; i < oldElements.length; i++) {
+        element = oldElements[i]
 
         // Set styles
-        element.style.animationDuration = transitionLengthSeconds;
-        element.style.animationDelay = '0s';
-        element.style.animationName = element.dataset.transition;
-        element.style.animationFillMode = 'forwards';
-        element.style.animationDirection = 'reverse';
+        element.style.animationDuration = transitionLengthSeconds
+        element.style.animationDelay = '0s'
+        element.style.animationName = element.dataset.transition
+        element.style.animationFillMode = 'forwards'
+        element.style.animationDirection = 'reverse'
       }
+    }, transitionTimeout)
 
-    }, transitionTimeout);
-
-    var x = this;
+    const x = this
     function done(x) {
-
       // Remove old container and add new one
-      x.oldContainer.style.visibility = 'hidden';
-      x.newContainer.style.visibility = 'visible';
+      x.oldContainer.style.visibility = 'hidden'
+      x.newContainer.style.visibility = 'visible'
 
       // Remove style tag at the end of the animation
-      setTimeout(function(){
-        document.body.style.overflow = 'visible';
-        for (var i = 0; i < allAnimationElements.length; i++) {
-          element = allAnimationElements[i];
-          element.removeAttribute('style');
+      setTimeout(() => {
+        document.body.style.overflow = 'visible'
+        for (let i = 0; i < allAnimationElements.length; i++) {
+          element = allAnimationElements[i]
+          element.removeAttribute('style')
         }
-      }, transitionLength);
+      }, transitionLength)
 
       // Scroll to top
-      document.body.scrollTop = 0;
+      document.body.scrollTop = 0
 
       // Done
-      x.done();
-
+      x.done()
     }
 
     // Mark as done
-    setTimeout(function(){
-      done(x);
-    }, transitionLength + transitionTimeout);
-
-  }
-});
+    setTimeout(() => {
+      done(x)
+    }, transitionLength + transitionTimeout)
+  },
+})
 
 // Add transition to barba
-Barba.Pjax.getTransition = function() {
-  return Transition;
-};
+Barba.Pjax.getTransition = () => Transition
 
-const progressBar = document.getElementById("js--progress");
+const progressBar = document.getElementById('js--progress')
 // const trackContent = document.querySelector("body");
-const push = document.getElementById("js--push");
-const body = document.body;
-push.style.marginLeft = 0;
-progressBar.style.width = '0%';
-// window.onscroll = function(event) {
-//   const pageHeight = window.innerHeight;
-//   const adjustedHeight = trackContent.clientHeight - pageHeight;
-//   const progress = ((window.pageYOffset / adjustedHeight) * 100);
-//
-//   console.log(pageHeight);
-//
-//   progressBar.style.width = `${progress}%`;
-//   push.style.marginLeft = `${progress}%`;
-// }
+const push = document.getElementById('js--push')
+const body = document.body
+push.style.marginLeft = 0
+progressBar.style.width = '0%'
 
-let progress = 0;
-let endPoint;
+let progress = 0
+let endPoint
 
-var setProgress = () => {
-    ticking = false;
-    try {
-        const y = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
-        progress = y / endPoint * 100;
-        progressBar.style.width = `${progress}%`;
-        push.style.marginLeft = `${progress}%`;
-    } catch (e) {
-        console.error(e);
-    }
-};
+const setProgress = () => {
+  ticking = false
+  try {
+    const y =
+      window.scrollY || window.pageYOffset || document.documentElement.scrollTop
+    progress = y / endPoint * 100
+    progressBar.style.width = `${progress}%`
+    push.style.marginLeft = `${progress}%`
+  } catch (e) {
+    console.error(e)
+  }
+}
 
 var setMetrics = () => {
-    endPoint = getEndPoint();
-    requestTick();
-};
+  endPoint = getEndPoint()
+  requestTick()
+}
 
-var getEndPoint = () => body.scrollHeight - (window.innerHeight || document.documentElement.clientHeight);
+var getEndPoint = () =>
+  body.scrollHeight -
+  (window.innerHeight || document.documentElement.clientHeight)
 
-setMetrics();
-window.addEventListener('scroll', onScroll, false);
-window.addEventListener('resize', setMetrics, false);
+setMetrics()
+window.addEventListener('scroll', onScroll, false)
+window.addEventListener('resize', setMetrics, false)
 
-var ticking = false;
+var ticking = false
 
 function requestTick() {
-	if(!ticking) {
-		requestAnimationFrame(setProgress);
-    requestAnimationFrame(scrollSwitch);
-	}
-	ticking = true;
+  if (!ticking) {
+    requestAnimationFrame(setProgress)
+    requestAnimationFrame(scrollSwitch)
+  }
+  ticking = true
 }
 // window.onscroll = () => {_setProgress()};
 // window.onresize = () => {_setMetrics()};
 
 function onScroll() {
-  requestTick();
+  requestTick()
 }
-
-function isMobileDevice() {
-    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
-};
